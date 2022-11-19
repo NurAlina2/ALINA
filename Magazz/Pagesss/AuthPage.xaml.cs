5a933a90e87545728870f185261c806850b3e7df
232014 <232014@WINMAC9>
2022-11-19 16:20:41 +0300

    Добавьте файлы проекта.

---
+++ Magazz/Pagesss/AuthPage.xaml.cs
@@ -0,0 +1,64 @@
+﻿using Magazz.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Magazz.Pagesss
+{
+    /// <summary>
+    /// Логика взаимодействия для AuthPage.xaml
+    /// </summary>
+    public partial class AuthPage : Page
+    {
+        public AuthPage()
+        {
+        //    //InitializeComponent();
+        //    //if (Properties.Settings.Default.Login != null);
+        //    //LoginTb.Text = Properties.Settings.Default.Login;
+        //    //if (Properties.Settings.Default.Password != null)
+        //    //    PasswordTb.Text = Properties.Settings.Default.Login;
+        //}
+
+        //private void RegistrationBtn_Click(object sender, RoutedEventArgs e)
+        //{
+        //    Navigation.NextPage(new Nav("Регистрация", new RegistPage()));
+        //}
+
+        //private void AuthBtn_Click(object sender, RoutedEventArgs e)
+        //{
+        //    string login = LoginTb.Text.Trim();
+        //    string passwword = PasswordTb.Text.Trim();
+        //    //if (login.Length == 0 && passwword.Length == 0) ;
+
+
+        //    //else
+        //    //{
+        //    //    Navigation.AuthUser = DBConnect.db.User.ToList().Find(x => x.Login == login && x.Password == passwword);
+
+        //    //    if (Navigation.AuthUser == null)
+        //    //        MessageBox.Show("Такого пользователя не существует ");
+        //    //    else
+        //    //    {
+
+        //    //        Navigation.isAuth = true;
+        //    //        Navigation.NextPage(new Nav(" Список услуг ", new ToyPage()));
+        //    //    }
+
+
+        //    //}
+
+
+        }
+    }
+}
